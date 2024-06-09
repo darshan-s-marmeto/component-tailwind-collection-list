d0fe555af9499174d4a1a78bc3517780798b5ae9
Darshan S <145565016+darshan-s-marmeto@users.noreply.github.com>
2024-06-09 07:28:40 +0530

    Initial files setup

---
+++ collection-list.js
@@ -0,0 +1,59 @@
+class collectionListSlider extends HTMLElement {
+  constructor() {
+    super();
+    this.splideInstance = null;
+  }
+
+  connectedCallback() {
+    this.initializeSplide();
+    window.addEventListener('resize', this.handleResize.bind(this));
+  }
+
+  disconnectedCallback() {
+    window.removeEventListener('resize', this.handleResize.bind(this));
+  }
+
+  initializeSplide() {
+    if (!this.splideInstance) {
+      this.classList.add('splide')
+      const _this = this;
+      const track = this.querySelector('.splide__track');
+      const list = this.querySelector('.splide__list');
+
+      if (track && list) {
+        const autoplay = this.getAttribute('data-autoplay') === 'true';
+        const interval = parseInt(this.getAttribute('data-interval'), 10) * 1000;
+        const pagination = this.getAttribute('data-pagination') === 'true';
+        const arrows = this.getAttribute('data-arrows') === 'true';
+        const desktopCount = this.getAttribute('data-desktopcount') || 3;
+        const mobileCount = this.getAttribute('data-mobilecount') || 2;
+        const type = autoplay? 'loop': 'slide';
+
+        console.log('count d', desktopCount, 'type', typeof(desktopCount))
+        console.log('count d', mobileCount, 'type', typeof(mobileCount))
+        this.splideInstance = new Splide(_this, {
+          type: type,
+          perPage: parseInt(desktopCount),
+          perMove: 1,
+          autoplay: autoplay,
+          interval: interval,
+          pagination: pagination,
+          arrows: arrows,
+          breakpoints: {
+            768: {
+              perPage: parseInt(mobileCount),
+            }
+          }
+        }).mount();
+      } else {
+        console.error('[splide] A track/list element is missing.');
+      }
+    }
+  }
+
+  handleResize() {
+    this.initializeSplide();
+  }
+}
+
+customElements.define('collection-list-slider', collectionListSlider);
